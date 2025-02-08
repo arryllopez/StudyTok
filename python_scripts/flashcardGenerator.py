d5b1d0ba6fa159c65fa51e1169109facfccce2ac
arryllopez <arryllopez7@gmail.com>
2025-02-08 12:59:12 -0500

    able to generate questions from long paragraphs

--- python_scripts/flashcardGenerator.py
+++ python_scripts/flashcardGenerator.py
@@ -1,4 +1,4 @@
-#using openAI api to generate flashcard questions
+#using openAI api to generate flashcard questions based on a general topic inputted by the user
 
 #"Due to the broad range of topics in the world, 
 # training an AI model was found to be difficult due to the shortage of 
@@ -10,6 +10,6 @@ from google import genai
 
 client = genai.Client(api_key="AIzaSyD8yibqQ-JEYma-fQWzYQHCmNjg7jp2vXs")
 response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works"
+    model="gemini-2.0-flash", contents="Generate 5 flashcard questions based on math"
 )
 print(response.text)
